3d70fa461402964bc6a40f33bded7ca83af0442c
Abayardo11 <abayardo2@ucol.mx>
2026-09-14 22:20:41 -0600

    commit

--- WifiOff.js
+++ WifiOff.js
@@ -1,35 +1,46 @@
-// Archivos que queremos guardar para usar sin internet
 let archivos = [
     "./",
-    "./Index.html",
-    "./Implementacion.json",
-    "./icon.svg"
+    "./index.html",
+    "./implementacion.json",
+    "./icon.svg",
+
+    "https://www.gstatic.com/firebasejs/12.19.0/firebase-app-compat.js",
+
+    "https://www.gstatic.com/firebasejs/12.19.0/firebase-database-compat.js"
 ];
 
-// Instalar el Service Worker
+
 self.addEventListener("install", function(event) {
 
     event.waitUntil(
-        caches.open("mi-lista").then(function(cache) {
+
+        caches.open("mi-lista-v2").then(function(cache) {
+
             return cache.addAll(archivos);
+
         })
+
     );
 
 });
 
-// Usar los archivos cuando no hay internet
+
 self.addEventListener("fetch", function(event) {
 
     event.respondWith(
+
         caches.match(event.request).then(function(respuesta) {
 
             if (respuesta) {
+
                 return respuesta;
+
             }
 
             return fetch(event.request);
+
         })
+
     );
 
 });
-
